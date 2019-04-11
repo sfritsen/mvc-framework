a56c2d9052fee592f26ea853d521853e25a9bb62
Stephen Fritsen <sfritsen@gmail.com>
2019-04-11 00:41:07 +0000

    SPL Autoloader Done

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ define('ENVIRONMENT', 'development');
 
 /*
  *---------------------------------------------------------------
- * ERROR REPORTING
+ * Error Reporting
  *---------------------------------------------------------------
  *
  * Different environments will require different levels of error reporting.
@@ -52,5 +52,3 @@ session_start();
 // Required files
 require_once __DIR__.'/core/AutoLoader.php';
 require_once __DIR__.'/app/web.php';
-
-new Route;
